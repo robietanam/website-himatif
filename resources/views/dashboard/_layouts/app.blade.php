b006467274b00a6a0b71b0dffd2decb5e70625da
robietanam <robietanam@gmail.com>
2024-04-27 12:47:13 +0700

    fix logout cannot press

--- resources/views/dashboard/_layouts/app.blade.php
+++ resources/views/dashboard/_layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,19 +17,22 @@
     <!-- Styles -->
     {{-- Bootstrap --}}
     <link href="{{ asset('vendors/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+
     {{-- Font Awesome --}}
     <link href="{{ asset('vendors/fontawesome/css/all.min.css') }}" rel="stylesheet">
     {{-- Bootstrap Select --}}
-    <link href="{{ asset('vendors/bs-select/bootstrap-select.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('vendors/bs-select/bootstrap-select.min.css') }}" rel="stylesheet">
     {{-- Sweetalert2 --}}
     <link rel="stylesheet" href="{{ asset('vendors/sweetalert2/sweetalert2.min.css') }}">
     {{-- Bootstrap Datepicker --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
     {{-- Apps --}}
     <link href="{{ asset('css/app-dashboard.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app-dashboard-components.min.css') }}" rel="stylesheet">
     @stack('style')
 </head>
+
 <body>
 
     <div id="app">
@@ -47,9 +51,9 @@
                     </div>
 
                     {{-- if flash alert exist --}}
-                    @if(Session::has('message'))
+                    @if (Session::has('message'))
                         <div class="alert alert-{{ Session::get('type') }}">
-                        {{ Session::get('message') }}
+                            {{ Session::get('message') }}
                         </div>
                     @endif
                     @yield('content')
@@ -71,12 +75,13 @@
     <script src="{{ asset('vendors/stisla/js/stisla.js') }}"></script>
     <script src="{{ asset('vendors/stisla/js/scripts.js') }}"></script>
     <script src="{{ asset('vendors/stisla/js/custom.js') }}"></script>
-    <script src="{{ asset('vendors/bs-select/bootstrap-select.min.js')}}"></script>
+    <script src="{{ asset('vendors/bs-select/bootstrap-select.min.js') }}"></script>
     {{-- Sweetalert2 --}}
     <script src="{{ asset('vendors/sweetalert2/sweetalert2.min.js') }}"></script>
     {{-- Bootstrap Datepicker --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-    <script src="{{ asset('js/app.js')}}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @stack('script')
 </body>
+
 </html>
--- resources/views/frontpage/layouts/app-frontpage.blade.php
+++ resources/views/frontpage/layouts/app-frontpage.blade.php
@@ -13,17 +13,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+    </script> --}}
 
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     {{-- Bootstrap --}}
     <link href="{{ asset('vendors/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('vendors/bootstrap/bootstrap.min.js') }}" rel="stylesheet">
     {{-- Font Awesome --}}
     <link href="{{ asset('vendors/fontawesome/css/all.min.css') }}" rel="stylesheet">
 
