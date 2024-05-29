482623845274297658237a0c0f65412a298dcda6
robietanam <robietanam@gmail.com>
2024-05-29 23:59:10 +0700

    HAPUS DUPLIKAT FONT

--- resources/views/frontpage/layouts/app-frontpage.blade.php
+++ resources/views/frontpage/layouts/app-frontpage.blade.php
@@ -10,8 +10,8 @@
     <link rel="icon" type="image/png" href="{{ asset('img/favicon-32x32.png') }}" sizes="32x32" />
     <link rel="icon" type="image/png" href="{{ asset('img/favicon-16x16.png') }}" sizes="16x16" />
 
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
-        rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet"> --}}
 
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script> --}}
     @vite(['resources/css/app-frontpage.css'])
