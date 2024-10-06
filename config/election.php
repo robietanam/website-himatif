c2a0419fa7f58fb093234209700754687b4fa735
robietanam <robietanam@gmail.com>
2024-10-06 09:40:04 +0700

    biar tidak perlu pull force

--- config/election.php
+++ config/election.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'campaign' => '2024-10-31 08:00:00', // Kampanye ketum
-    'start' => '2024-10-30 08:00:00',
-    'end' => '2024-10-06 17:00:00',
+    'campaign' => env('CAMPAIGN', '2024-10-29 08:00:00'), // Kampanye ketum
+    'start' => env('START', '2024-10-30 08:00:00'),
+    'end' => env('END', '2024-10-31 08:00:00'),
 ];
